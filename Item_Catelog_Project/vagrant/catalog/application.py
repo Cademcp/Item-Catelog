82a1edd1b18bc46be66da3c00877c1910ca25261
Cade McPartlin <cademcp@gmail.com>
2018-07-29 23:20:32 -0500

    formatted code

--- Item_Catelog_Project/vagrant/catalog/application.py
+++ Item_Catelog_Project/vagrant/catalog/application.py
@@ -1,15 +1,8 @@
-from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
-
-
+from flask import Flask, render_template, request, redirect, jsonify, url_for, flash, request, make_response
 from flask import session as login_session
-import random
-import string
 
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.client import FlowExchangeError
-import httplib2
-from flask import make_response, request, jsonify
-import requests
 
 from sqlalchemy import create_engine, inspect
 from sqlalchemy.orm import sessionmaker
@@ -22,7 +15,6 @@ engine = create_engine('sqlite:///item_catelog.db')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
-
 
 
 @app.route('/')
@@ -69,24 +61,7 @@ def editItem(category, item):
     if request.method == 'GET':
         return render_template('editItem.html')
 
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host = '0.0.0.0', port=5000)
-
-# TODO DB CRUD code for later
-# engine = create_engine('sqlite:///item_catelog.db')
-# Base.metadata.bind = engine
-
-# DBSession = sessionmaker(bind=engine)
-
-# session = DBSession()
-
-# category = session.query(Category.name).all()
-
-# for i in category:
-#     print i.name
-
-# category = session.query(Item.name).all()
-
-# for i in category:
-#     print i.name
